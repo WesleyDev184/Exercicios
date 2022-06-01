0320aca2e204a749b51a508f8ae9617182d0cdcc
WesleyDev <57929638+WesleyDev184@users.noreply.github.com>
2022-06-01 18:44:21 -0300

    Atualizaçao

--- ExercicioPromice/src/index.js
+++ ExercicioPromice/src/index.js
@@ -2,9 +2,9 @@ import Spaceship from './spaceship'
 import SpaceshipEngine from "./spaceshipEngine"
 
 
-const sophia = new Spaceship("Sophia", 10, 5)
-const amsterda = new Spaceship("Amsterdã", 14, 10)
-const dwarfStart = new Spaceship("Estrela-Anã", 20, 4)
+const sophia = new Spaceship("Sophia", 10, 5, 70)
+const amsterda = new Spaceship("Amsterdã", 14, 10, 40)
+const dwarfStart = new Spaceship("Estrela-Anã", 20, 4, 80)
 
 const sophiaEngine = new SpaceshipEngine(sophia)
 const amsterdaEngine = new SpaceshipEngine(amsterda)
--- ExercicioPromice/src/spaceship.js
+++ ExercicioPromice/src/spaceship.js
@@ -1,11 +1,12 @@
 class Spaceship {
-    constructor(name, maxCapacity, currentCharge) {
+    constructor(name, maxCapacity, currentCharge, shield) {
       this.name = name
       this.maxCapacity = maxCapacity
-      this.currentChage = currentCharge
+      this.currentCharge = currentCharge
+      this.shield = shield
     }
     currentPercentCharge() {
-      return this.currentChage * 100 / this.maxCapacity
+      return this.currentCharge * 100 / this.maxCapacity
     }
   }
   export default Spaceship
